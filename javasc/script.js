b0650b0ede407c3cdec743421c63bac093c1f4db
Jeftry <trycoba452@gmail.com>
2024-02-19 11:35:08 +0700

    membuat profile

---
+++ javasc/script.js
@@ -0,0 +1,13 @@
+//halo dunia//
+alert('halo dunia')
+
+//key
+
+let firstName = 'joko';
+firstName = 'dolo';
+console.log(firstName);
+
+const a =10;
+const b = 5;
+const c = a * b;
+console.log(c);
